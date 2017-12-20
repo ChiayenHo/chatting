08b7415977367ff5939e579047032efbb81111c1
vivian8637 <vivian8637@gmail.com>
2017-12-20 21:07:56 +0800

    2
    2

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var linebot = require('linebot');
 var express = require('express');
+const http = require('http');
+
 //使用套件
 
 var bot = linebot({
@@ -16,8 +18,27 @@ bot.on('message', function(event) {
   const linebotParser = bot.parser();
   app.post('/', linebotParser);
   
+var server = http.createServer(function(req, res) {
+    
+    var resHeader = {
+        'Accept-Charset': 'utf-8',
+        'Accept-Language': 'zh-TW',
+        'Content-Type': 'text/html; charset=utf-8',
+    }
+    
+    res.writeHead(200, resHeader)
+    res.write('<h1>Hello</h1>', 'utf8');
+    res.write('<p>這是由 node.js 建立的 WebServer</p>', 'utf8');
+    res.end();
+});
+    
+// 將 Server 開啟 port 1234 執行起來
+//server.listen(1234);
+//console.log('Server running at http://127.0.0.1:1234');
+
   //因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-  var server = app.listen(process.env.PORT || 8080, function() {
+    server.listen(process.env.PORT || 8080, function() {
+
     var port = server.address().port;
     console.log("App now running on port", port);
   });
